fac5fd4837df58edcd7461489937041f5630bb83
kryparnold <emirhanpgt@gmail.com>
2024-02-07 07:28:53 +0300

    implemented home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import SearchBar from "@/components/SearchBar";
+import TodoList from "@/components/TodoList";
+import { useTodo } from "@/components/context/TodoProvider";
+import { useState } from "react";
+
 export default function Home() {
+    const [content, setContent] = useState("");
+    const { addTodo } = useTodo();
+
     return (
-        <main>
-            Home
+        <main className="w-full flex justify-center">
+            <MaxWidthWrapper>
+                <div className="flex gap-5 py-10">
+                    <SearchBar filter={content} setFilter={(filter: string) => setContent(filter)} />
+                    <button type="submit" value={content} className="bg-sky-500 rounded-xl p-3 text-2xl whitespace-nowrap" onClick={() => {
+                        addTodo(content);
+                        setContent("");
+                    }}>Add +</button>
+                </div>
+                <TodoList filter={content} />
+            </MaxWidthWrapper>
         </main>
     );
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,13 +2,14 @@ import MaxWidthWrapper from "./MaxWidthWrapper";
 
 interface SearchBarProps {
     setFilter: (filter: string) => void;
+    filter: string;
 }
 
-export default function SearchBar({ setFilter }: SearchBarProps) {
+export default function SearchBar({ setFilter, filter }: SearchBarProps) {
     return (
         <div className="w-full grid place-items-center">
             <div className="w-full flex">
-                <input type="text" className="border-none outline-none my-10 bg-zinc-700 p-4 text-xl rounded-xl flex-1" placeholder="Todo Content..." onChange={(e) => setFilter(e.target.value)} />
+                <input type="text" value={filter} className="border-none outline-none bg-zinc-700 p-4 text-xl rounded-xl flex-1" placeholder="Todo Content..." onChange={(e) => setFilter(e.target.value)} />
             </div>
         </div>
     );
